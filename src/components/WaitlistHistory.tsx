1b8c4d5eb79caec8fda506b1bc8fa3feebe6394f
Haris Iftikhar <hiftikh@uwo.ca>
2024-11-19 00:58:55 -0500

    Added CVS button

--- src/components/WaitlistHistory.tsx
+++ src/components/WaitlistHistory.tsx
@@ -1,13 +1,11 @@
-import React from "react";
+import { cn } from "@/lib/utils";
+import { Button } from "./ui/button";
 import usePlayerStore from "@/hooks/usePlayerStore";
 import { colorVariantsTag } from "@/lib/utils";
 
 import {
   Table,
   TableBody,
-  TableCaption,
-  TableCell,
-  TableFooter,
   TableHead,
   TableHeader,
   TableRow,
@@ -17,6 +15,33 @@ import { Badge } from "@/components/ui/badge";
 
 export default function WaitlistHistory() {
   const { waitList } = usePlayerStore((state) => state);
+
+  const onClickBtnHandle = () => {
+    const waitListData = waitList
+      .map((entry) => {
+        let playersList;
+        const formatTime = new Date(entry.timeStamp?.toString() as string);
+        const formatedTime = `${formatTime.toLocaleTimeString()}`;
+        const formatedDate = `${formatTime.toLocaleDateString()}`;
+        playersList = entry.players
+          ?.map((player) => player.display_name)
+          .join(", ");
+        return `${formatedDate},${formatedTime},${playersList}`;
+      })
+      .join("\n");
+
+    const csvContent =
+      "data:text/csv;charset=utf-8,Date,Start Time,Player1,Player2,Player3,Player4\n" +
+      waitListData;
+
+    const encodedUri = encodeURI(csvContent);
+    const link = document.createElement("a");
+    link.setAttribute("href", encodedUri);
+    link.setAttribute("download", "waitlist_history.csv");
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
 
   const waitlistData = waitList.map((list, index) => {
     const formatTime = new Date(list.timeStamp?.toString() as string);
@@ -67,6 +92,16 @@ export default function WaitlistHistory() {
           </TableHeader>
           <TableBody>{waitlistData}</TableBody>
         </Table>
+      )}
+      <br />
+      {waitList.length !== 0 && (
+        <Button
+          disabled={!waitList}
+          className="mx-auto text-center"
+          onClick={onClickBtnHandle}
+        >
+          Save History as CSV
+        </Button>
       )}
     </>
   );
