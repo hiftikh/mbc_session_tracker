9a5f181b6021fca26fcbe7a8e768544067ef2f25
Haris Iftikhar <hiftikh@uwo.ca>
2024-11-19 15:42:13 -0500

    fixed popover width

--- src/components/AddPlayer.tsx
+++ src/components/AddPlayer.tsx
@@ -71,7 +71,7 @@ export default function AddPlayer() {
             <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
           </Button>
         </PopoverTrigger>
-        <PopoverContent className="w-full p-0">
+        <PopoverContent>
           <Command>
             <CommandInput placeholder="Search Player..." />
             <CommandList>
