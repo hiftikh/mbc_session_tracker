38e4fcdab62a337118c143c6ee658005c98a1093
Haris Iftikhar <hiftikh@uwo.ca>
2024-11-19 00:57:37 -0500

    Created ResetButton with toast

---
+++ src/components/ResetButton.tsx
@@ -0,0 +1,53 @@
+import usePlayerStore from "@/hooks/usePlayerStore";
+import { useToast } from "@/hooks/use-toast";
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Button } from "@/components/ui/button";
+
+export default function ResetButton() {
+  const { resetAll } = usePlayerStore((state) => state);
+  const { toast } = useToast();
+
+  const onClickHandle = () => {
+    resetAll();
+    toast({
+      title: "Completed",
+      description: "Waitlist Team and Waitlist History have been reset.",
+      className: "bg-green-100",
+      duration: 2000,
+    });
+  };
+
+  return (
+    <>
+      <AlertDialog>
+        <AlertDialogTrigger asChild>
+          <Button>Reset All</Button>
+        </AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+            <AlertDialogDescription>
+              This action cannot be undone. This will permanently delete your
+              Waitlist Team and Waitlist History section.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction onClick={onClickHandle}>Reset</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </>
+  );
+}
